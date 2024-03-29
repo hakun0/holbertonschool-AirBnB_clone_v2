f2d044c41ed5496ca30672804e11b106215cd971
hakun0 <brithon.saniko@gmail.com>
2024-03-29 23:57:28 +0100

    rere

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,79 +1,50 @@
 #!/usr/bin/python3
-"""Defines the class DBStorage"""
-from sqlalchemy import create_engine
-from os import getenv
-from sqlalchemy.orm import sessionmaker
-from models.base_model import BaseModel, Base
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.user import User
-from models.state import State
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.ext.declarative import declarative_base
+"""This module defines a class to manage file storage for hbnb clone"""
+import json
 
 
-class DBStorage:
-    """Representes a class DBStorage"""
+class FileStorage:
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
+    __objects = {}
 
-    __engine = None
-    __session = None
-
-    def __init__(self):
-        """initialize the instance of DBStorage"""
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}"
-                                      .format(getenv("HBNB_MYSQL_USER"),
-                                              getenv("HBNB_MYSQL_PWD"),
-                                              getenv("HBNB_MYSQL_HOST"),
-                                              getenv("HBNB_MYSQL_DB")),
-                                      pool_pre_ping=True)
-
-        if getenv("HBNB_ENV") == "test":
-            Base.metadata.drop_all(self.__engine)
-
-    def all(self, cls=None):
-        """Query on the current database session all objects
-        depending of the class name.
-        """
-        all_class = [City, State, User, Place, Review, Amenity]
-        obj_list = []
-        new_dict = {}
-
-        if cls is None:
-            for i in range(len(all_class)):
-                obj_list += self.__session.query(all_class[i]).all()
-        else:
-            obj_list += self.__session.query(cls).all()
-
-        for obj in obj_list:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            new_dict[key] = obj
-
-        return new_dict
+    def all(self):
+        """Returns a dictionary of models currently in storage"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """Add the object to the current database session"""
-        self.__session.add(obj)
+        """Adds new object to storage dictionary"""
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Commit all changes of the current database session"""
-        self.__session.commit()
-
-    def delete(self, obj=None):
-        """Delete from the current database session obj if not None"""
-        if obj is not None:
-            self.__session.delete(obj)
+        """Saves storage dictionary to file"""
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
 
     def reload(self):
-        """Create all tables in the database"""
-        Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
-    def close(self):
-        """call remove() method on the private session attribute"""
-        self.__session.close()
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        try:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                        self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
+            pass
--- models/state.py
+++ models/state.py
@@ -1,34 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
-from os import getenv
-import models
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    """ State class inherits from BaseModel and Base
-    name (string): state name
-    """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
-
-    @property
-    def cities(self):
-        """Returns the list of City instances"""
-        if type(models.storage).__name__ == "DBStorage":
-            city_list = []
-            for city in self.cities:
-                city_list.append(city)
-            return city_list
-
-        else:
-            city_list = []
-            cities = models.storage.all(City)
-            for city in cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+class State(BaseModel):
+    """ State class """
+    name = ""
